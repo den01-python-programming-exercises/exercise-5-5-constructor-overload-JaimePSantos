2b627fd2e6a0ab4aae131aa4f99ba2d2469b6155
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-28 19:07:48 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
+from product import Product
 def main():
     #write your code below this line
+  tape_measure = Product("Tape measure")
+  plaster = Product.with_location("Plaster", "home improvement section")
+  tyre = Product.with_weight("Tyre", 5)
 
+  print(tape_measure)
+  print(plaster)
+  print(tyre)
+  pass
 if __name__ == '__main__':
     main()
--- src/product.py
+++ src/product.py
@@ -4,3 +4,14 @@ class Product:
         self.name = name
         self.location = location
         self.weight = weight
+    
+    @classmethod
+    def with_location(cls,name,location):
+      return cls(name,location)
+    
+    @classmethod
+    def with_weight(cls,name,weight):
+      return cls(name,"shelf",weight)
+    
+    def __str__(self):
+      return "%s (%s kg) can be found from the %s"%(self.name,self.weight,self.location)
